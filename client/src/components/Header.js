18b04d867038f8812cbe3b23c312a4207aeac001
Giuseppe Rotolo <pepu.rotolo@gmail.com>
2019-06-07 09:51:54 +0200

    handling of login and logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent(){
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return(
       <nav>
         <div className="nav-wrapper">
           <a href="#" className="brand-logo">Emaily</a>
           <ul id="nav-mobile" className="right">
-            <li>
-              <a href="sass.html">Login With Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
